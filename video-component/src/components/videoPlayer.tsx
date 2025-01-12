beb1e27bd6bc561c1d47d97e4b6d8ada4adad77d
SANGHYEOP <127101419+ohsanghyeop@users.noreply.github.com>
2025-01-13 00:43:38 +0900

    feat 반복재생 고침
    :ㅂ

--- video-component/src/components/videoPlayer.tsx
+++ video-component/src/components/videoPlayer.tsx
@@ -15,9 +15,10 @@ const Player: React.FC<PlayerProps> = ({ src, autoplay = false }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
-  const [volume, setVolume] = useState(1); // 볼륨 상태
-  const [showControls, setShowControls] = useState(true); // 컨트롤 표시 상태
-  const hideControlsTimeoutRef = useRef<NodeJS.Timeout | null>(null); // 타이머 참조
+  const [volume, setVolume] = useState(1);
+  const [mousePosition, setMousePosition] = useState<
+    "top" | "middle" | "bottom" | null
+  >(null);
 
   useEffect(() => {
     if (videoRef.current) {
@@ -44,15 +45,28 @@ const Player: React.FC<PlayerProps> = ({ src, autoplay = false }) => {
     }
   }, [src, autoplay]);
 
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    const { clientY, currentTarget } = event;
+    const height = currentTarget.offsetHeight;
+
+    if (clientY < height * 0.3) {
+      setMousePosition("top");
+    } else if (clientY > height * 0.7) {
+      setMousePosition("bottom");
+    } else {
+      setMousePosition("middle");
+    }
+  };
+
   const togglePlayPause = () => {
     if (videoRef.current) {
       if (isPlaying) {
         videoRef.current.pause();
-        setIsPlaying(false); // 일시정지 상태 업데이트
+        setIsPlaying(false);
       } else {
         videoRef.current
           .play()
-          .then(() => setIsPlaying(true)) // 재생 상태 업데이트
+          .then(() => setIsPlaying(true))
           .catch((error) => console.error("Error playing video:", error));
       }
     }
@@ -75,16 +89,24 @@ const Player: React.FC<PlayerProps> = ({ src, autoplay = false }) => {
   const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newVolume = parseFloat(event.target.value);
     if (videoRef.current) {
-      videoRef.current.volume = newVolume; // 비디오 볼륨 변경
+      videoRef.current.volume = newVolume;
     }
-    setVolume(newVolume); // 상태 업데이트
+    setVolume(newVolume);
   };
 
   const handleVideoEnded = () => {
     if (videoRef.current) {
-      videoRef.current.currentTime = 0; // 처음으로 되감기
-      videoRef.current.play(); // 비디오 자동 재생
-      setIsPlaying(true); // 재생 상태 업데이트
+      setIsPlaying(true);
+      videoRef.current.currentTime = 0;
+      setTimeout(() => {
+        videoRef.current
+          ?.play()
+          .then(() => console.log("Auto-play started successfully"))
+          .catch((error) => {
+            console.error("Error during auto-play:", error);
+            setIsPlaying(false);
+          });
+      }, 50);
     }
   };
 
@@ -97,16 +119,6 @@ const Player: React.FC<PlayerProps> = ({ src, autoplay = false }) => {
     )}`;
   };
 
-  const handleMouseMove = () => {
-    setShowControls(true); // 컨트롤 표시
-    if (hideControlsTimeoutRef.current) {
-      clearTimeout(hideControlsTimeoutRef.current); // 기존 타이머 초기화
-    }
-    hideControlsTimeoutRef.current = setTimeout(() => {
-      setShowControls(false); // 1초 후 컨트롤 숨기기
-    }, 1000);
-  };
-
   return (
     <div
       className="relative bg-black rounded-lg overflow-hidden shadow-lg"
@@ -114,7 +126,7 @@ const Player: React.FC<PlayerProps> = ({ src, autoplay = false }) => {
         width: `${videoDimensions.width}px`,
         height: `${videoDimensions.height}px`,
       }}
-      onMouseMove={handleMouseMove} // 마우스 이동 시 컨트롤 표시
+      onMouseMove={handleMouseMove}
     >
       {/* Video Element */}
       <video
@@ -122,54 +134,51 @@ const Player: React.FC<PlayerProps> = ({ src, autoplay = false }) => {
         className="w-full h-full object-contain"
         onTimeUpdate={handleTimeUpdate}
         onClick={togglePlayPause}
-        onEnded={handleVideoEnded} // 자동 재생 처리
+        onEnded={handleVideoEnded}
+        autoPlay={autoplay}
       />
 
       {/* Central Play/Pause Button */}
-      <button
-        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/70 text-kakao-yellow text-[3rem] rounded-full w-[6.25rem] h-[6.25rem] flex items-center justify-center hover:bg-black/50 transition-opacity duration-1000 ${
-          showControls
-            ? "opacity-100 pointer-events-auto"
-            : "opacity-0 pointer-events-none"
-        }`}
-        onClick={togglePlayPause}
-      >
-        {isPlaying ? "⏸︎" : "▶"}
-      </button>
+      {mousePosition === "middle" && (
+        <button
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/70 text-kakao-yellow text-[3rem] rounded-full w-[6.25rem] h-[6.25rem] flex items-center justify-center hover:bg-black/50 transition-opacity duration-1000"
+          onClick={togglePlayPause}
+        >
+          {isPlaying ? "⏸︎" : "▶"}
+        </button>
+      )}
 
       {/* Custom Controls */}
-      <div
-        className={`absolute bottom-0 left-0 right-0 text-white p-2 transition-opacity duration-1000 ${
-          showControls ? "opacity-100" : "opacity-0"
-        }`}
-      >
-        <div className="flex items-center space-x-4">
-          {/* Progress Bar */}
-          <input
-            type="range"
-            className="w-full h-1 bg-transparent rounded-lg outline-none accent-kakao-yellow"
-            min="0"
-            max={duration || 0}
-            step="0.1"
-            value={currentTime}
-            onChange={handleSeek}
-          />
-
-          {/* Volume Control */}
-          <div className="flex items-center space-x-2">
-            <span>🔊</span>
+      {mousePosition === "bottom" && (
+        <div className="absolute bottom-0 left-0 right-0 text-white p-2 transition-opacity duration-1000 opacity-100">
+          <div className="flex items-center space-x-4">
+            {/* Progress Bar */}
             <input
               type="range"
-              className="h-1 w-24 accent-kakao-yellow"
+              className="w-full h-1 bg-transparent rounded-lg outline-none accent-kakao-yellow"
               min="0"
-              max="1"
-              step="0.01"
-              value={volume}
-              onChange={handleVolumeChange}
+              max={duration || 0}
+              step="0.1"
+              value={currentTime}
+              onChange={handleSeek}
             />
+
+            {/* Volume Control */}
+            <div className="flex items-center space-x-2">
+              <span>🔊</span>
+              <input
+                type="range"
+                className="h-1 w-24 accent-kakao-yellow"
+                min="0"
+                max="1"
+                step="0.01"
+                value={volume}
+                onChange={handleVolumeChange}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
